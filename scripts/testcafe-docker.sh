ab4d1e3eb592a02b5658b4046c2b988ac1cc265b
Annamaria Szegedi <aszegedi@hortonworks.com>
2018-05-13 21:10:42 +0200

    Introduce environment variable check for CI

--- scripts/testcafe-docker.sh
+++ scripts/testcafe-docker.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -x
 
-XVFB_SCREEN_WIDTH=${SCREEN_WIDTH-1280}
-XVFB_SCREEN_HEIGHT=${SCREEN_HEIGHT-720}
-
 cd $HOME
 chmod -Rf 777 $HOME
 
@@ -11,10 +8,16 @@ yarn install
 yarn check
 
 dbus-daemon --session --fork
-Xvfb :1 -screen 0 "${XVFB_SCREEN_WIDTH}x${XVFB_SCREEN_HEIGHT}x24" >/dev/null 2>&1 &
+Xvfb :1 -screen 0 1920x1080x24 >/dev/null 2>&1 &
 export DISPLAY=:1.0
 fluxbox >/dev/null 2>&1 &
 sleep 20
+
+sudo -E -u testcafe \
+    echo "BASE_URL: " $BASE_URL \
+    echo "USERNAME: " $USERNAME \
+    echo "PASSWORD: " $PASSWORD
+
 sudo -E -u testcafe node_modules/.bin/testcafe --ports 1337,1338 "$@"
 export RESULT=$?
 
