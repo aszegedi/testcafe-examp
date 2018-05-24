305e883ee0e075560a13ab37a025acb01ae18e23
Annamaria Szegedi <aszegedi@hortonworks.com>
2018-05-24 18:25:47 +0200

    Fixing Runner

--- runner.ts
+++ runner.ts
@@ -10,37 +10,44 @@ import * as fs from 'fs';
 const testcafeBrowserTools = require('testcafe-browser-tools');
 const createTestCafe       = require('testcafe');
 const stream               = fs.createWriteStream('result.xml');
-let runner                 = null;
 let chromeInfo             = null;
+let testcafe               = null;
 
 testcafeBrowserTools
     .getBrowserInfo(`${CHROME_BIN}`)
-    .then(res => {
-        chromeInfo = res;
+    .then(browserInfo => {
+        chromeInfo = browserInfo;
 
-        console.log(chromeInfo);
         chromeInfo.cmd = `${chromeInfo.cmd} --no-sandbox --disable-web-security --start-maximized`;
     })
-    .then(() => createTestCafe()).then(testcafe => {
-        runner = testcafe.createRunner();
+    .then(() => createTestCafe(null, 1337, 1338))
+    .then(tc => {
+        console.log(chromeInfo);
 
-        return runner.src([
-            './tmp/tests/loginTest.js',
-            './tmp/tests/baseTest.js',
-            './tmp/tests/credentialTest.js',
-            './tmp/tests/clusterTest.js',
-            './tmp/tests/storageTest.js'
-        ]).browsers(chromeInfo)
-          .reporter('xunit', stream)
-          .screenshots('results/screenshots', true)
-          .run({
-            selectorTimeout: 20000,
-            assertionTimeout: 120000,
-            pageLoadTimeout: 60000,
-            speed: 0.5
-        }).then(failedCount => {
-            console.log('Tests failed: ' + failedCount);
-            testcafe.close();
-        });
+        testcafe = tc;
+        const runner = testcafe.createRunner();
 
-});
+        return runner
+            .src([
+                './tmp/tests/loginTest.js',
+                './tmp/tests/baseTest.js',
+                './tmp/tests/credentialTest.js',
+                './tmp/tests/clusterTest.js',
+                './tmp/tests/storageTest.js'
+            ])
+            .browsers(chromeInfo)
+            .reporter('spec')
+            .reporter('xunit', stream)
+            .screenshots('results/screenshots', true)
+            .run({
+                selectorTimeout: 20000,
+                assertionTimeout: 120000,
+                pageLoadTimeout: 60000,
+                speed: 0.5
+            })
+            .then(failedCount => {
+                console.log('Tests failed: ' + failedCount);
+                return testcafe.close();
+            });
+    });
+
